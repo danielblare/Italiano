a2c10399eac3d9122cedd1aea383b9e1644ab095
Danylo Siefierov <93935347+stuffeddanny@users.noreply.github.com>
2023-10-19 18:57:33 +0100

    Delivery option selector

--- Italiano/Cart/CartView.swift
+++ Italiano/Cart/CartView.swift
@@ -8,10 +8,24 @@
 import SwiftUI
 import SwiftData
 
+struct Line: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: 0, y: 0))
+            path.addLine(to: CGPoint(x: rect.width, y: 0))
+        }
+    }
+}
+
+
 struct CartView: View {
+    enum DeliveryOption {
+        case delivery, pickup
+    }
     @Environment(\.modelContext) private var context
     
     @Query private var items: [CartItem]
+    @State private var deliveryOption: DeliveryOption = .delivery
     
     var body: some View {
         VStack {
@@ -37,23 +51,10 @@ struct CartView: View {
             }
             
             VStack {
-                Divider()
-                    .frame(height: 1.5)
-                    .overlay(Color.palette.lightGreen)
+                GreenDivider
                     .padding(.vertical)
-
-                HStack {
-                    Text("Delivery")
-                    
-                    Divider()
-                        .frame(width: 1.5, height: 30)
-                        .overlay(Color.palette.oliveGreen)
-                        .padding(.horizontal)
-
-                    Text("Pick Up")
-                }
-                .foregroundStyle(Color.palette.oliveGreen)
-                .font(.asset.heading2)
+                
+                DeliveryOptionSelector
                 
                 TextField(text: .constant("")) {
                     Label("", systemImage: "cart")
@@ -61,20 +62,59 @@ struct CartView: View {
                 .padding()
                 .background(.secondary.opacity(0.3))
                 
-                Divider()
-                    .frame(height: 1.5)
-                    .overlay(Color.palette.lightGreen)
+                GreenDivider
                     .padding(.vertical)
 
-                
-                
-                
                 Spacer()
             }
             .padding(.horizontal)
         }
         .navigationTitle("Cart")
         .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    private var DeliveryOptionSelector: some View {
+        HStack {
+            VStack(spacing: 0) {
+                Text("Delivery")
+                
+                if deliveryOption == .delivery {
+                    Line()
+                        .stroke(style: .init(lineWidth: 1, dash: [4]))
+                        .frame(width: 80, height: 1)
+                }
+            }
+            .onTapGesture {
+                deliveryOption = .delivery
+            }
+            
+            Divider()
+                .frame(width: 1.5, height: 30)
+                .overlay(Color.palette.oliveGreen)
+                .padding(.horizontal)
+            
+            VStack(spacing: 0) {
+                Text("Pick Up")
+                
+                if deliveryOption == .pickup {
+                    Line()
+                        .stroke(style: .init(lineWidth: 1, dash: [4]))
+                        .frame(width: 80, height: 1)
+                }
+            }
+            .onTapGesture {
+                deliveryOption = .pickup
+            }
+        }
+        .foregroundStyle(Color.palette.oliveGreen)
+        .font(.asset.heading2)
+        .animation(.smooth, value: deliveryOption)
+    }
+    
+    private var GreenDivider: some View {
+        Divider()
+            .frame(height: 1.5)
+            .overlay(Color.palette.lightGreen)
     }
 }
 

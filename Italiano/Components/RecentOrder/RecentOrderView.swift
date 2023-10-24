36714370fa949706f4feb2536f50186d7766af5a
Danylo Siefierov <93935347+stuffeddanny@users.noreply.github.com>
2023-10-24 16:07:12 +0100

    Recent order view, some unit tests

---
+++ Italiano/Components/RecentOrder/RecentOrderView.swift
@@ -0,0 +1,18 @@
+//
+//  RecentOrderView.swift
+//  Italiano
+//
+//  Created by Daniel on 10/24/23.
+//
+
+import SwiftUI
+
+struct RecentOrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecentOrderView()
+}
---
+++ ItalianoTests/CacheTests.swift
@@ -0,0 +1,20 @@
+//
+//  CacheTests.swift
+//  ItalianoTests
+//
+//  Created by Daniel on 10/24/23.
+//
+
+import XCTest
+@testable import Italiano
+
+final class CacheTests: XCTestCase {
+
+    override func setUp() {
+        
+    }
+    
+    override class func tearDown() {
+        
+    }
+}
